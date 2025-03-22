28f0aa91d502d2e763512c075fe9890ecd54f9ad
Stefan Itic <114070283+ITic5@users.noreply.github.com>
2025-03-22 12:08:01 +0100

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,15 +1,28 @@
-import { getWeather } from "./weatherApp.js";
+import {getForecast, getWeather} from "./weatherApp.js";
+import {changeLocation, cityName, getForecastButton} from "../script";
 
 export async function checkStoredCity() {
     try {
         let storedCity = localStorage.getItem("city");
-        let changeLocation = document.getElementById("change-location");
 
         if (storedCity) {
+
+            cityName.value = storedCity;
             changeLocation.classList.remove("hidden");
+            getForecastButton.classList.remove("hidden");
+
+
             getWeather(storedCity);
+
+            getForecastButton.removeEventListener("click", handleForecastClick);
+            getForecastButton.addEventListener("click", handleForecastClick);
         }
     }catch (exception) {
         console.log("Something went wrong with getting stored city checked.");
     }
+}
+
+function handleForecastClick() {
+    cityName.value = localStorage.getItem("city");
+    getForecast(localStorage.getItem("city"));
 }
